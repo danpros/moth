f2fd9bdb256b323caf17595f21c29aae2c2fb398
danpros <dan@danpros.com>
2021-03-13 07:36:38 +0700

    Update static.html.php

--- static.html.php
+++ static.html.php
@@ -4,6 +4,9 @@
 	<header class="post__header">
 		<h1 class="post__title" style="margin-bottom:20px;"><?php echo $p->title;?>
 		</h1><!-- /.post__title -->
+		<span class="post__date">
+		<?php if (login()) { echo '<span><a href="'. $p->url .'/edit?destination=post">Edit</a></span>'; } ?>
+		</span>
 	</header><!-- /.post__header -->
 
 	<div class="post__content">
